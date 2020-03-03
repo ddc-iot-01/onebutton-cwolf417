009380da09f84d50bdba45d5a66f93cf9e4c86ac
Clint <cwolf2>
2020-03-03 15:33:37 -0700

    because I didn't want to lose my stuff

--- 05_oneButton.ino/05_oneButton.ino.ino
+++ 05_oneButton.ino/05_oneButton.ino.ino
@@ -1,46 +1,106 @@
 /*
  * Project: Using OneButton library
  * Description: Introduct Students to OneButton Library using a button and the serial monitor
- * Author: 
- * Date: 
+ * Author: Clint Wolf
+ * Date: 3-3-2020
+ * Button 13 with a pull-down resisitor
  */
 
-#include "OneButton.h"
+#include <OneButton.h>
 
-// Setup OneButton on pin 23
+// Setup OneButton on pin 23 // Pin 23 can handle analog read and write
+OneButton button1(23, false);
 
 // Create variables buttonState and flash
+int ledDelay = 500; // 1000 ms = 1 s
+int buttonState = LOW;
+int flash = LOW;
+int ledPinGreen = 5; // the Green LED is drawing power from Pin 5
 
+// ------------------------------------------- VOID SETUP FUNCTION
 
-void setup() {
-
+void setup() 
+{
 // Setup Serial Monitor
+Serial.begin(9600);
+while (!Serial);
+
+pinMode(ledPinGreen, OUTPUT);
+
 // Link oneButton library to functions click1, doubleclick1, longPressStart1
-oneButton button1(23,false);
+
+button1.attachClick(click1);
+button1.attachDoubleClick(doubleclick1);
+//button1.attachLongPressStart(longPressStart1);
+//button1.attachLongPressStop(longPressStop1);
+button1.setClickTicks(250);
+button1.setPressTicks(1000);
 } 
 
-void loop() {
+//======================================================================================
+
+void loop() 
+{
   // keep watching the push buttons:
   button1.tick();
-} // loop
+  //Serial.print ("buttonState=");
+  //Serial.println (buttonState);
+  //Serial.print ("flash=");
+  //Serial.println (flash);
+}
 
 
 // ----- button 1 callback functions
 
+
+//======================================================================================
+
+void click1()
 // This function will be called when the button1 was pressed 1 time.
-void click1() {
+{
 //change and print buttonState
-  
-} // click1
+  Serial.println("Button 1 single click");
+  buttonState = !buttonState;
+  Serial.print("buttonState = ");
+  Serial.println (buttonState);
+  //Serial.println(buttonState);
+  // Now, make the LED flash
+  digitalWrite(ledPinGreen,buttonState); // turn LED ON or OFF
+}
 
+//======================================================================================
 
+void doubleclick1()
 // This function will be called when the button1 was pressed 2 times in a short timeframe.
-void doubleclick1() {
+{
 // change state of flash and print
-} // doubleclick1
 
+  Serial.println("Button 1 double clicked");
+  flash = !flash;
+  Serial.print("flash = ");
+  Serial.println(flash);
+  digitalWrite(ledPinGreen,buttonState);
+}
 
+  
+//======================================================================================  
+
+void longPressStart1()
 // This function will be called once, when the button1 is pressed for a long time.
-void longPressStart1() {
+{
 // print longPress
-} // longPressStart1
+
+Serial.println("Button 1 long press");
+
+}
+
+//======================================================================================  
+
+// This function will be called once, when the button1 is long pressed, then released to go on vacation.
+void longPressStop1() 
+{
+// print longPress
+
+Serial.println("Button 1 long press stop");
+
+} // longPressStop1
